7d4d72f44f8037eaa186b34361acd55d62f1c1c7
simersbpl <simersbpl@gmail.com>
2023-04-26 12:25:29 +0530

    final website

---
+++ script.js
@@ -0,0 +1,39 @@
+gsap.from("#home-content-h", {duration: 2.5, x: -700, ease: "ease",
+// repeat: -1,
+delay: 1,
+// yoyo: true,
+// rotate:("30deg"),
+fontWeight: 700,
+margin: 2,
+padding: 2
+})
+
+
+
+gsap.from("#of-companies", { 
+    duration: 2.5,
+    // backgroundColor: '#8d3dae',
+    // rotate:("360deg"),
+    x: -700,
+    delay: 1.5,
+    ease: "linear"
+
+  });
+
+// gsap.from('#home-content-h', {
+//   duration: 2.5,
+//   delay: 3.5,
+//   x: 1500,
+//   scale: 3,
+//   color: green,
+// })
+
+
+gsap.from("#what-we-do", { 
+    duration: 3.5,
+    // backgroundColor: '#8d3dae',
+    // rotate:("360deg"),
+    x: -900,
+    delay: 3.5,
+    ease: "linear"
+  });
